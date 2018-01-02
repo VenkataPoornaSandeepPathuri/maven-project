83b78c8d9764b06b10edb1aaae0b3e6559eb5bc5
unknown <cse@sitecselab314.www.sasi.com>
2018-01-02 22:46:12 +0530

    Added Comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /*
+
+	Helo World
+	*/
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
